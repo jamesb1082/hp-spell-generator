1784e1cc61ba69370e258bbbbdbea8b0cd4a6a62
jamesb1082 <jamesbrill16@gmail.com>
2016-12-13 17:23:58 +0000

    testing test2

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
 import sys
 sys.path.insert(0, '../hp_spells/hp_spells')
 import pytest
-from hp_spells import contains
+from hp_spells import *
 
 
 if __name__ == '__main__':
-	assert contains("james", "_") == False 
+
+	#Test 1 checks whether the string contains a character
+	assert contains("james", "_") == False
+
+	#Test 2  
+	input1 = [
+			["type 1", "lang 1", 2], 
+			["type 1", "lang 2", 2], 
+			["type 2", "lang 1", 2], 
+			["type 2", "lang 2", 2], 
+	]
+
+	output = [
+			["type 1", "lang 1", 2, 0.25], 
+			["type 1", "lang 2", 2, 0.25], 
+			["type 2", "lang 1", 2, 0.25], 
+			["type 2", "lang 2", 2, 0.25],
+	]
+
+	assert calcProb(input1) == output
+	
