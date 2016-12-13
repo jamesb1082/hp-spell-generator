402402a47ca40aa55c43c0dd9b2884b0f6dc2efa
jamesb1082 <jamesbrill16@gmail.com>
2016-12-13 19:53:43 +0000

    This contains tests 1-4 with relevant try catches.

--- test.py
+++ test.py
@@ -4,11 +4,19 @@ import pytest
 from hp_spells import *
 
 
+def failedtest(num, name):
+	str1 = "test " + str(num) +  ": " + str(name) 
+	str2 = "result = failed" 
+	
+	print("{0:<50} {1:<50}".format(str1, str2)) 
+	
 if __name__ == '__main__':
-
+	
 	#Test 1 checks whether the string contains a character
-	assert contains("james", "_") == False
-
+	try:
+		assert contains("james", "_") == False
+	except: 
+		failedtest(1, "contains()") 
 	#Test 2  
 	input1 = [
 			["type 1", 2, "lang 1"], 
@@ -23,11 +31,22 @@ if __name__ == '__main__':
 			["type 2", 2, "lang 1", 0.25], 
 			["type 2", 2, "lang 2", 0.25],
 	]
-	assert calcProb(input1) == output
+
+	try: 
+		assert calcProb(input1) == output
+	except: 
+		failedtest(2, "calcProb()")
 
 
 	#test 3 language code
-	assert langCode("latin") == "la" 	
+	try:
+		assert langCode("latin") == "la" 	
+	except: 
+		failedtest(3, "langCode()") 
 
+	
 	#test 4 pig latin function 
-	assert pigLatin("this is a test") == "isthay isway away esttay"
+	try: 
+		assert pigLatin("this is a test") == "histay isay aay esttay"
+	except: 
+		failedtest(3, "pigLatin()") 
