41fd4b70df4dabf880e912430f68090d22dbd991
James Brill <jamesbrill16@gmail.com>
2017-04-18 20:24:00 +0100

    further documentation

--- eval.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, "/hp_spells")
-from hp_spells import hp_spells
-
-model = hp_spells.load("../vectors/GoogleNews-vectors-negative300.bin")  
-
-print hp_spells.generateSpell("open the door quietly")
